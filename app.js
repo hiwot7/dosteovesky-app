3ea7afcb5ce89eec913e0892c5fffde753a9d9cc
hiwot7 <tesfafikadenat@gmail.com>
2026-05-02 09:22:02 +0300

    Added Dostoevsky quotes array

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // DosteoveskyQuote Logic 
+const quotes = [
+    "To live without hope is to cease to live.",
+    "The mystery of human existence lies not in just staying alive, but in finding something to live for.",
+    "Beauty will save the world.",
+
+],
+    randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
